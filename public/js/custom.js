6e5f9e56a294c0e97ad0a6ea4210491e43dbc4bd
Shaun Montante <shaunmontante@gmail.com>
2020-08-09 11:49:05 -0700

    added main content in page

--- public/js/custom.js
+++ public/js/custom.js
@@ -39,3 +39,64 @@ btn.addEventListener('click', (e) => {
     alert('Thank you, our team will contact you shortly!');
 
 });
+
+let covidApidata;
+
+const url = 'https://api.covid19api.com/summary';
+fetch(url) 
+.then(data=>{return data.json()})
+.then(res=>{
+
+covidApidata = res;
+console.log(res);
+
+let search = (key, inputArray) => {
+  for (let i = 0; i < inputArray.length; i++) {
+      if (inputArray[i].Country === key) {
+          return inputArray[i];
+      }
+  }
+}
+
+let chinaResults = search('China', covidApidata.Countries);
+let italyResults = search('Italy', covidApidata.Countries);
+let usaResults = search('United States of America', covidApidata.Countries);
+let spainResults = search('Spain', covidApidata.Countries);
+let germanyResults = search('Germany', covidApidata.Countries);
+let franceResults = search('France', covidApidata.Countries);
+let iranResults = search('Iran', covidApidata.Countries);
+
+countryData = [
+  usaData = {
+    country: usaResults.Country,
+    total: usaResults.TotalConfirmed
+  },
+  italyData = {
+    country: italyResults.Country,
+    total: italyResults.TotalConfirmed
+  },
+  chinaData = {
+    country: chinaResults.Country,
+    total: chinaResults.TotalConfirmed
+  },
+  spainData = {
+    country: spainResults.Country,
+    total: spainResults.TotalConfirmed
+  },
+    germanyData = {
+      country: germanyResults.Country,
+      total: germanyResults.TotalConfirmed
+  },
+  franceData = {
+    country: franceResults.Country,
+    total: franceResults.TotalConfirmed
+  }
+];
+  document.getElementById('usa').innerHTML = countryData[0].country + ' Covid Cases: ' + '<i style="color:#99D315" class="fas fa-chevron-up"></i>' + countryData[0].total;
+  document.getElementById('italy').innerHTML = countryData[1].country + ' Covid Cases: ' + '<i style="color:#99D315" class="fas fa-chevron-up"></i>' + countryData[1].total;
+  document.getElementById('china').innerHTML = countryData[2].country + ' Covid Cases: ' + '<i style="color:#99D315" class="fas fa-chevron-up"></i>' + countryData[2].total;
+  document.getElementById('spain').innerHTML = countryData[3].country + ' Covid Cases: ' + '<i style="color:#99D315" class="fas fa-chevron-up"></i>' + countryData[3].total;
+  document.getElementById('germany').innerHTML = countryData[4].country + ' Covid Cases: ' + '<i style="color:#99D315" class="fas fa-chevron-up"></i>' + countryData[4].total;
+  document.getElementById('france').innerHTML = countryData[5].country + ' Covid Cases: ' + '<i style="color:#99D315" class="fas fa-chevron-up"></i>' + countryData[5].total;
+})
+.catch(error=>console.log(error));
